edf124cfd691cc20373ce9d7901983c8683974c5
mrgoboom <mrgoboom@gmail.com>
2015-07-28 23:18:27 -0700

    Entity defined

--- ShiftDown.vhd
+++ ShiftDown.vhd
@@ -2,3 +2,12 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity shiftDown is
+	port(
+	--input: x, the number to be shifted
+	--	 n, the number of bits to shift to the right
+	--output: xn, shifted down, rounding instead of truncating
+		x: IN std_logic_vector(15 downto 0);
+		n: IN std_logic_vector(15 downto 0);
+		xn: OUT std_logic_vector(15 downto 0));
+
+
